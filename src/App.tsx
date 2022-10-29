097677d1bbb92f9723eac563e561c0f80c1186d4
ajaythapliyal <ajaythapliyal1703@gmail.com>
2022-10-30 01:00:59 +0530

    takes input from user which atom to render

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.scss';
 import { Canvas, useThree} from '@react-three/fiber'
 import Atom from './Atom/Atom';
 import { OrbitControls } from '@react-three/drei';
+import { Menu } from './Menu/Menu';
+import { AtomName } from './models';
 
 function App() {
+
+  const [atom, setAtom] = useState<AtomName>('hydrogen')
+
   return (
   <>
   <div className={styles.header}><p className={styles['header-title']}>KALP</p></div>
     <div className={styles.app}>
       <Canvas>
         <ambientLight intensity={0.1} />
-        <Atom name='magnesium'></Atom>
+        <Atom name={atom}></Atom>
         <OrbitControls></OrbitControls>
         </Canvas>
     </div>
+    <Menu atom={atom} onAtomChange={(atom : AtomName)=> setAtom(atom)}></Menu>
   </>)
 }
 
