19a3225e804935c35dde1466d7267345b8d91cc4
ajaythapliyal <ajaythapliyal1703@gmail.com>
2022-10-30 09:21:11 +0530

    removes unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styles from './App.module.scss';
-import { Canvas, useThree} from '@react-three/fiber'
+import { Canvas} from '@react-three/fiber'
 import Atom from './Atom/Atom';
 import { OrbitControls } from '@react-three/drei';
 import { Menu } from './Menu/Menu';
