f6d0110c6f30bf488612cd63077eb15a83b0bcc3
ajaythapliyal <ajaythapliyal1703@gmail.com>
2022-12-04 18:35:44 +0530

    WIP

--- src/Autocomplete/Autocomplete.tsx
+++ src/Autocomplete/Autocomplete.tsx
@@ -3,18 +3,21 @@ import styles from './Autocomplete.module.scss'
 
 export function Autocomplete({defaultItem, items, onItemSelect, placeholder} : {defaultItem : string,  items: readonly string[], onItemSelect: (item : string) => void, placeholder?: string }){
     const [item, setItem] = useState(defaultItem);
+    const [isDirty, setIsDirty] = useState(false);
     const  getFilteredItems = (searchItem : string) => items.filter(item =>  searchItem.length && item.includes(searchItem) )
     const onSuggestionClick = (item : string)=>{
-        onItemSelect(item);
         setItem(item);
+        setIsDirty(false)
+        onItemSelect(item);
     }
-    const isSuggestionsVisible = (item : string, filteredItems: string[])=> {
-        return !(filteredItems.length === 1 && filteredItems[0] === item)
+    const handleChange =(searchItem : string) =>{
+        setItem(searchItem.trim().toLowerCase());
+        setIsDirty(true);
     }
-
+    
     const filteredItems = getFilteredItems(item);
     return <div>
-            <input value={item} onChange={(event)=> setItem(event.target.value)} placeholder={placeholder} className={styles.input}></input>
-            { isSuggestionsVisible(item, filteredItems) && filteredItems.map(filteredItem => <div onClick={()=> {onSuggestionClick(filteredItem)}} className={styles.suggestion} key={filteredItem}><p>{filteredItem}</p></div>)}
+            <input value={item} onChange={(event)=> handleChange(event.target.value)} placeholder={placeholder} className={styles.input}></input>
+            { isDirty && filteredItems.map(filteredItem => <div onClick={()=> {onSuggestionClick(filteredItem)}} className={styles.suggestion} key={filteredItem}><p>{filteredItem}</p></div>)}
         </div>
 }
